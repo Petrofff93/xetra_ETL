81370746bc54773b697897b7e269b51256b3030f
Petar Petrov <ppetrov@roiti.com>
2022-10-25 15:43:52 +0300

    implemented the return_date_list method

--- tests/common/test_meta_process.py
+++ tests/common/test_meta_process.py
@@ -7,6 +7,7 @@ from moto import mock_s3
 import pandas as pd
 
 from xetra.common.constants import MetaProcessFormat
+from xetra.common.custom_exceptions import WrongMetaFileException
 from xetra.common.meta_process import MetaProcess
 from xetra.common.s3 import S3BucketConnector
 
@@ -30,11 +31,14 @@ class TestMetaProcessMethods(unittest.TestCase):
         self.s3_bucket_name = 'test_bucket'
         # Creating a bucket on the mocked s3
         self.s3 = boto3.resource(service_name='s3', endpoint_url=self.s3_endpoint_url)
-        self.s3.create_bucket(Bucket=self.s3_bucket_name, CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
+        self.s3.create_bucket(Bucket=self.s3_bucket_name,
+                              CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
         self.s3_bucket = self.s3.Bucket(self.s3_bucket_name)
         # Creating a S3BucketConnector instance
-        self.s3_bucket_meta = S3BucketConnector(self.s3_access_key, self.s3_secret_key, self.s3_endpoint_url, self.s3_bucket_name)
-        self.dates = [(datetime.today().date() - timedelta(days=day)).strftime(MetaProcessFormat.META_DATE_FORMAT.value) for day in range(8)]
+        self.s3_bucket_meta = S3BucketConnector(self.s3_access_key, self.s3_secret_key, self.s3_endpoint_url,
+                                                self.s3_bucket_name)
+        self.dates = [(datetime.today().date() - timedelta(days=day)).strftime(MetaProcessFormat.META_DATE_FORMAT.value)
+                      for day in range(8)]
 
     def tearDown(self) -> None:
         # mocking s3 connection stop
@@ -62,7 +66,7 @@ class TestMetaProcessMethods(unittest.TestCase):
         self.assertEqual(date_list_exp, date_list_result)
         self.assertEqual(proc_date_list_exp, proc_date_list_result)
         # Cleanup after test
-        self.s3_bucket.delete_objects(Delete={'Objects': [{'key': meta_key}]})
+        self.s3_bucket.delete_objects(Delete={'Objects': [{'Key': meta_key}]})
 
     def test_update_meta_file_empty_date_list(self):
         """
@@ -89,9 +93,9 @@ class TestMetaProcessMethods(unittest.TestCase):
         when there already a meta file
         """
         # Expected results
-        date_list_old = ['2022-10-12', '2022-10-13']
-        date_list_new = ['2022-10-16', '2022-10-17']
-        date_list_exp = date_list_new + date_list_old
+        date_list_old = ['2022-10-16', '2022-10-17']
+        date_list_new = ['2022-10-12', '2022-10-13']
+        date_list_exp = date_list_old + date_list_new
         proc_date_list_exp = [datetime.today().date()] * 4
         # Test init
         meta_key = 'meta.csv'
@@ -106,3 +110,50 @@ class TestMetaProcessMethods(unittest.TestCase):
         self.s3_bucket.put_object(Body=meta_content, Key=meta_key)
         # Method execution
         MetaProcess.update_meta_file(date_list_new, meta_key, self.s3_bucket_meta)
+        # Read meta file
+        data = self.s3_bucket.Object(key=meta_key).get().get('Body').read().decode('utf-8')
+        out_buffer = StringIO(data)
+        df_meta_result = pd.read_csv(out_buffer)
+        date_list_result = list(df_meta_result[MetaProcessFormat.META_SOURCE_DATE_COL.value])
+        proc_date_list_result = list(pd.to_datetime(df_meta_result[MetaProcessFormat.META_PROCESS_COL.value]).dt.date)
+        # Test after method execution
+        self.assertEqual(date_list_exp, date_list_result)
+        self.assertEqual(proc_date_list_exp, proc_date_list_result)
+        # Cleanup after test
+        self.s3_bucket.delete_objects(Delete={
+            'Objects': [
+                {
+                    'Key': meta_key
+                }
+            ]
+        })
+
+    def test_update_meta_file_meta_file_wrong(self):
+        """
+        Tests the update_meta_file method
+        when there is a wrong meta file
+        """
+        # Expected results
+        date_list_old = ['2022-10-12', '2022-10-13']
+        date_list_new = ['2022-10-16', '2022-10-17']
+        # Test init
+        meta_key = 'meta.csv'
+        meta_content = (
+            f"wrong_column,{MetaProcessFormat.META_PROCESS_COL.value}\n"
+            f"{date_list_old[0]},"
+            f"{datetime.today().strftime(MetaProcessFormat.META_PROCESS_DATE_FORMAT.value)}\n"
+            f"{date_list_old[1]},"
+            f"{datetime.today().strftime(MetaProcessFormat.META_PROCESS_DATE_FORMAT.value)}"
+        )
+        self.s3_bucket.put_object(Body=meta_content, Key=meta_key)
+        # Method execution
+        with self.assertRaises(WrongMetaFileException):
+            MetaProcess.update_meta_file(date_list_new, meta_key, self.s3_bucket_meta)
+        # Cleanup after test
+        self.s3_bucket.delete_objects(Delete={
+            "Objects": [
+                {
+                    'Key': meta_key
+                }
+            ]
+        })
